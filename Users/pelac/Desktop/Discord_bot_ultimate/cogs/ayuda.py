4c987e94d0a4c7d7ea97cab5a80e8c183aee4a9c
Pelacrack27 <pelayo.muniz@gmail.com>
2022-07-15 00:05:20 +0200

    make it better

--- Users/pelac/Desktop/Discord_bot_ultimate/cogs/ayuda.py
+++ Users/pelac/Desktop/Discord_bot_ultimate/cogs/ayuda.py
@@ -16,7 +16,6 @@ class Ayuda(commands.Cog):
   async def ayuda(self, ctx):
     embed=discord.Embed(title="Ayuda",  description="A continuación se muestran los multisummons diponibles", color=discord.Color.blue())
     embed.set_author(name=ctx.author.display_name,  icon_url=ctx.author.avatar_url)
-    embed.add_field(name="!probs", value="Comprueba las probabilidades de los multisummons", inline=False)
     embed.add_field(name="!multimonos", value="Multisummon de goku y vegeta ssj4 septimo aniversario", inline=False)
     embed.add_field(name="!multigods", value="Multisummon de goku y vegeta ssj god septimo aniversario", inline=False)
     embed.set_footer(text="Muchas gracias por usar el bot!")
--- Users/pelac/Desktop/Discord_bot_ultimate/main.py
+++ Users/pelac/Desktop/Discord_bot_ultimate/main.py
@@ -4,7 +4,6 @@ from discord.ext import commands
 
 client = commands.Bot(command_prefix="!")
 
-os.chdir(os.path.dirname(__file__))
 
 @client.event
 async def on_ready():
